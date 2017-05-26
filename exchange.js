080446a53ba39c98df13c742541f30b44707bf04
nedievas <nedievas@outlook.com>
2017-05-26 10:06:41 +0300

    Add files via upload

---
+++ exchange.js
@@ -0,0 +1,160 @@
+var BFX = require('bitfinex-api-node')
+  , _ = require('lodash')
+  , path = require('path')
+  , n = require('numbro')
+
+module.exports = function container (get, set, clear) {
+  var c = get('conf')
+  
+  var public_client, authed_client
+
+  function publicClient () {
+    if (!public_client) public_client = new BFX.APIRest()
+    return public_client
+  }
+
+  function authedClient () {
+    if (!authed_client) {
+    if (!c.bitfinex.key || c.bitfinex.key === 'YOUR-API-KEY') {
+      throw new Error('please configure your Bitfinex credentials in ' + path.resolve(__dirname, 'conf.js'))
+    }
+    authed_client = new BFX.APIRest(c.bitfinex.key, c.bitfinex.secret)
+  }
+  return authed_client
+  }
+  
+  function joinProduct (product_id) {
+    return product_id.split('-')[0] + '' + product_id.split('-')[1]
+  }
+
+  return {
+    name: 'bitfinex',
+//    historyScan: 'backward',
+    makerFee: 0.1,
+
+    getProducts: function () {
+      return require('./products.json')
+    },
+
+    getTrades: function (opts, cb) {
+      var client = publicClient()      
+      var args = joinProduct(opts.product_id)
+//      var path = args.pair;
+//      if(opts.from)
+//        path += '?limit_trades=49999';
+/*
+ if (opts.from) {
+        args.start = opts.from
+      }
+      if (opts.to) {
+        args.end = opts.to
+      }
+      if (args.start && !args.end) {
+        // add 24 hours
+        args.end = args.start + 86400
+      }
+      
+      ****
+      if (opts.from) {
+        // move cursor into the future
+        args.before = opts.from
+      }
+      else if (opts.to) {
+        // move cursor into the past
+        args.after = opts.to
+      }
+*/
+      client.trades(args, function (err, body) {
+        if (err) return cb(err)
+        var trades = body.map(function(trade) {
+          return {
+            trade_id: trade.tid,
+            time: n(trade.timestamp).multiply(1000).value(),
+            size: parseFloat(trade.amount),
+            price: parseFloat(trade.price),
+            side: trade.type
+          }
+        })
+        cb(null, trades)
+      })
+    },
+
+    getBalance: function (opts, cb) {
+      var client = authedClient()
+      client.wallet_balances(function (err, body) {
+        if (err) return cb(err)
+        var balance = {asset: 0, currency: 0}
+        var accounts = _(body).filter(function (body) { return body.type === c.bitfinex.wallet }).forEach(function (account) {
+          if (account.currency.toUpperCase() === opts.currency) {
+            balance.currency = account.amount
+            balance.currency_hold = (account.amount - account.available)
+          }
+          else if (account.currency === opts.asset) {
+            balance.asset = account.amount
+            balance.asset_hold = (account.amount - account.available)
+          }
+        })
+        cb(null, balance)
+      })
+    },
+
+    getQuote: function (opts, cb) {
+      var client = publicClient()
+      var pair = joinProduct(opts.product_id)
+      client.ticker(pair, function (err, body) {
+        if (err) return cb(err)
+        cb(null, {bid: body.bid, ask: body.ask})
+      })
+    },
+
+    cancelOrder: function (opts, cb) {
+      var client = authedClient()
+      client.cancel_order(opts.order_id, function (err, body) {
+        if (err) return cb(err)
+        cb()
+      })
+    },
+
+    buy: function (opts, cb) {
+      var client = authedClient()
+            if (typeof opts.type === 'undefined') {
+		    opts.type = 'exchange limit'
+	    }
+      var pair = joinProduct(opts.product_id)
+      client.new_order(pair, opts.size, opts.price, 'bitfinex', 'buy', opts.type, false, function (err, body) {
+        if (err) return cb(err)
+        cb(null, body)
+      })
+//	    console.log(pair, opts)
+    },
+
+
+    sell: function (opts, cb) {
+      var client = authedClient()
+	    if (typeof opts.type === 'undefined') {
+		    opts.type = 'exchange limit'
+	    }
+      var pair = joinProduct(opts.product_id)
+      client.new_order(pair, opts.size, opts.price, 'bitfinex', 'sell', opts.type, false, function (err, body) {
+         if (err) return cb(err)
+	 cb(null, body)
+      })
+//	    console.log(pair, opts)
+    },
+
+    getOrder: function (opts, cb) {
+      var client = authedClient()
+      client.order_status(opts.order_id, function (err, body) {
+        if (err) return cb(err)
+        cb(null, body)
+      })
+//	    console.log(opts)
+    },
+
+
+    // return the property used for range querying.
+    getCursor: function (trade) {
+      return trade.trade_id
+    }
+  }
+}
